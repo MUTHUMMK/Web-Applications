dd6e93498ec18c81c48b498aecbe12b5fc93238a
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-07-17 21:06:59 +0530

    Add files via upload

---
+++ NETFLIX/python.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.DataFrame({
+    'Name': ['Luke','Gina','Sam','Emma'],
+    'Status': ['Father', 'Mother', 'Son', 'Daughter'],
+    'Birthyear': [1976, 1984, 2013, 2016],
+})
